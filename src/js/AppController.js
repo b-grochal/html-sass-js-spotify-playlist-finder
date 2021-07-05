78244d34d99d1e732aab92c9cd42a8c2b62e34b6
Bartosz <radu480@o2.pl>
2021-07-05 23:42:18 +0200

    Created custom select.

--- src/js/AppController.js
+++ src/js/AppController.js
@@ -13,6 +13,7 @@ export default class AppController {
   init() {
     console.log("Running the app.");
     this.loadCategories();
+    this.configureSearchPlaylistSection();
   }
 
   async loadCategories() {
@@ -20,5 +21,34 @@ export default class AppController {
     localStorage.setItem("token", token);
     const categories = await this.#apiController.getCategories(token);
     this.#uiController.insertCategories(categories);
+  }
+
+  configureSearchPlaylistSection() {
+    document
+      .querySelector(".custom-select-wrapper")
+      .addEventListener("click", function () {
+        this.querySelector(".custom-select").classList.toggle("open");
+      });
+
+    for (const option of document.querySelectorAll(".custom-option")) {
+      option.addEventListener("click", function () {
+        if (!this.classList.contains("selected")) {
+          this.parentNode
+            .querySelector(".custom-option.selected")
+            .classList.remove("selected");
+          this.classList.add("selected");
+          this.closest(".custom-select").querySelector(
+            ".custom-select__trigger .custom-select__text"
+          ).textContent = this.textContent;
+        }
+      });
+    }
+
+    window.addEventListener("click", function (e) {
+      const select = document.querySelector(".custom-select");
+      if (!select.contains(e.target)) {
+        select.classList.remove("open");
+      }
+    });
   }
 }
