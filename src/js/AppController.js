e5e1168367993ac83b56ab035dd5356e5a182b55
Bartosz <radu480@o2.pl>
2021-07-07 15:19:48 +0200

    Fixed error with selecting categories.

--- src/js/AppController.js
+++ src/js/AppController.js
@@ -10,9 +10,9 @@ export default class AppController {
     this.#apiController = new ApiController();
   }
 
-  init() {
+  async init() {
     console.log("Running the app.");
-    this.loadCategories();
+    await this.loadCategories();
     this.configureSearchPlaylistSection();
   }
 
@@ -32,10 +32,14 @@ export default class AppController {
 
     for (const option of document.querySelectorAll(".custom-option")) {
       option.addEventListener("click", function () {
+        console.log("CLICK");
         if (!this.classList.contains("selected")) {
-          this.parentNode
-            .querySelector(".custom-option.selected")
-            .classList.remove("selected");
+          const selectedOption = this.parentNode.querySelector(
+            ".custom-option.selected"
+          );
+          if (selectedOption) {
+            selectedOption.classList.remove("selected");
+          }
           this.classList.add("selected");
           this.closest(".custom-select").querySelector(
             ".custom-select__trigger .custom-select__text"
@@ -44,11 +48,11 @@ export default class AppController {
       });
     }
 
-    window.addEventListener("click", function (e) {
-      const select = document.querySelector(".custom-select");
-      if (!select.contains(e.target)) {
-        select.classList.remove("open");
-      }
-    });
+    // window.addEventListener("click", function (e) {
+    //   const select = document.querySelector(".custom-select");
+    //   if (!select.contains(e.target)) {
+    //     select.classList.remove("open");
+    //   }
+    // });
   }
 }
