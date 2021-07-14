dafa518f27a40c63c1de571a0edc0dc748ad17e9
Bartosz <radu480@o2.pl>
2021-07-14 19:28:28 +0200

    Chnaged anonymous funtions to arrow functions.

--- src/js/AppController.js
+++ src/js/AppController.js
@@ -10,10 +10,11 @@ export default class AppController {
     this.#apiController = new ApiController();
   }
 
-  async init() {
+  init() {
     console.log("Running the app.");
-    await this.loadCategories();
-    this.configureSearchPlaylistSection();
+    this.loadCategories().then(() => {
+      this.configureSearchPlaylistSection();
+    });
   }
 
   async loadCategories() {
@@ -26,29 +27,31 @@ export default class AppController {
   configureSearchPlaylistSection() {
     document
       .querySelector(".custom-select-wrapper")
-      .addEventListener("click", function () {
-        this.querySelector(".custom-select").classList.toggle("open");
+      .addEventListener("click", (e) => {
+        document.querySelector(".custom-select").classList.toggle("open");
       });
 
     for (const option of document.querySelectorAll(".custom-option")) {
-      option.addEventListener("click", function () {
+      option.addEventListener("click", (e) => {
         console.log("CLICK");
-        if (!this.classList.contains("selected")) {
-          const selectedOption = this.parentNode.querySelector(
+        if (!e.target.classList.contains("selected")) {
+          const selectedOption = e.target.parentNode.querySelector(
             ".custom-option.selected"
           );
           if (selectedOption) {
             selectedOption.classList.remove("selected");
           }
-          this.classList.add("selected");
-          this.closest(".custom-select").querySelector(
-            ".custom-select__trigger .custom-select__text"
-          ).textContent = this.textContent;
+          e.target.classList.add("selected");
+          e.target
+            .closest(".custom-select")
+            .querySelector(
+              ".custom-select__trigger .custom-select__text"
+            ).textContent = this.textContent;
         }
       });
     }
 
-    window.addEventListener("click", function (e) {
+    window.addEventListener("click", (e) => {
       const select = document.querySelector(".custom-select");
       if (!select.contains(e.target)) {
         select.classList.remove("open");
