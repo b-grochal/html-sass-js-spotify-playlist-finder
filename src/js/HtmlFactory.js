0ef98f51d5b40818ae5ac79b41754547e46a64a1
Bartosz <radu480@o2.pl>
2021-07-15 00:55:48 +0200

    Implemented factory method for details of playlist.

--- src/js/HtmlFactory.js
+++ src/js/HtmlFactory.js
@@ -29,4 +29,32 @@ export default class HtmlFactory {
       "</section>",
     ].join("\n");
   }
+
+  generatePlaylistDetailsSection(playlist, tracks) {
+    const tracksHtml = [];
+    tracks.forEach((track) =>
+      tracksHtml.push(
+        `
+      <a class="playlist-details__track">
+        <span class="playlist-details__track-description">${track.track.name} - ${track.track.artists[0].name}</span>
+      </a>
+      `
+      )
+    );
+
+    return [
+      '<section class="playlist-detials">',
+      '<header class="playlist-details__header">',
+      '<img class="playlist-details__playlist-image">',
+      '<h2 class="playlist-details__playlist-name>',
+      "</header>",
+      '<section class="playlist-details__tracks>',
+      ...tracksHtml,
+      "</section>",
+      '<section class="playlist-details__controls>',
+      '<button class="playlist-details__btn>Back</button>',
+      "</section>",
+      "</section>",
+    ];
+  }
 }
